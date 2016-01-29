43064ae0b5b4d7876ba3730077539577b54895c3
xubin <onireuso@163.com>
2016-01-29 15:14:33 +0800

    本地工程修改适配
    1 修改republish方式，否则npm本地编译会失败
    2 修改展示页面，去除无用功能
    3 部分说明汉化

--- res/app/control-panes/control-panes-controller.js
+++ res/app/control-panes/control-panes-controller.js
@@ -10,12 +10,12 @@ module.exports =
         templateUrl: 'control-panes/screenshots/screenshots.jade',
         filters: ['native', 'web']
       },
-      {
-        title: gettext('Automation'),
-        icon: 'fa-road color-lila',
-        templateUrl: 'control-panes/automation/automation.jade',
-        filters: ['native', 'web']
-      },
+      // {
+      //   title: gettext('Automation'),
+      //   icon: 'fa-road color-lila',
+      //   templateUrl: 'control-panes/automation/automation.jade',
+      //   filters: ['native', 'web']
+      // },
       {
         title: gettext('Advanced'),
         icon: 'fa-bolt color-brown',
